31e2add0e5c2cb2d8a55b70511f51129c9fe9942
LucasxMenezes <menezesavelino@gmail.com>
2024-08-15 23:41:10 -0300

    welcome1

---
+++ livro/src/welcome1.java
@@ -0,0 +1,9 @@
+// Fig. 2.1: Welcome.java
+// Um primeiro programa em Java
+
+public class welcome1 {
+    public static void main(String[] args) {
+        System.out.println("Wellcome to Java Programming" );
+    }
+
+}
